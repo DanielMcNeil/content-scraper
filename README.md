9522d9210f1408e96eb2f8bb2bf6b1c5c9e27689
DanielMcNeil <daniel.mcneil@outlook.com>
2016-10-30 16:00:44 -0400

    added code comments, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # content-scraper
+Full Stack JavaScript Techdegree Project 6
+
+A web crawler that gets t-shirt data from the http://shirts4mike.com website once per 24 hours.
+
+**notes**
+
+I used the x-ray module for the web crawler, fast-csv for the csv writer, and async for iterating the tshirt links.
+
+each of these modules have active development (multiple releases, with the latest release being fairly recently).  Most of these have multiple contributors.  They also have fairly good (for the most part) documentation.
+
+I used the async module to ensure that all of the data that I needed was collected before being passed to the function that writes the csv.  (asynchronous execution is definatly a double-edged sword)
+
+I used the JSHint module when running 'npm run lint'.  The '|| true' in the package.json file supresses the npm error message that is thrown when JSHint finds syntax errors.  The JSHint error message provides all of the necessary information, the additional NPM message is unnecessary (the NPM error message does go away if you don't have any JavaScript syntax errors).
--- scraper.js
+++ scraper.js
@@ -1,8 +1,11 @@
+// dependencies
 var Xray = require('x-ray');
 var csv = require('fast-csv');
 var fs = require('fs');
 var async = require('async');
-var shirtData = [];
+
+// xray options
+// this one slices the price off of the beginning of the tshirt title, since price is captured separatly
 var xray = Xray({
   filters:{
     slice: function (value, start , end) {
@@ -11,41 +14,11 @@ var xray = Xray({
   }
 });
 
-var getShirtLinks = function() {
-  xray('http://www.shirts4mike.com/shirts.php', '.products', ['a@href'])(function(error,data) {
-      if (data) {
-        getShirtData(data);
-      } else {
-        console.log("error getting t-shirt links: " + error);
-      }
-    }
-  );
-};
+// array to hold tshirt data
+var shirtData = [];
 
-var getShirtData = function(links) {
-  async.each(links, shirtDataCall, function(err) {
-    exportToCSV(shirtData);
-  });
-};
-
-var shirtDataCall = function(link, finished) {
-  xray(link, {
-        image: '.shirt-picture img@src',
-        price: '.price',
-        title: '.shirt-details h1 | slice:4'
-      })(function(error,data) {
-        if (data) {
-          data.url = link;
-          shirtData.push(data);
-          return(finished(null));
-        } else {
-          console.log("error getting t-shirt data: " + error);
-        }
-      }
-    );
-};
-
-var exportToCSV = function(object) {
+// get current date-time
+var getDateTime = function() {
   var today = new Date();
   var month = today.getMonth() + 1;
   var day = today.getDate();
@@ -56,27 +29,99 @@ var exportToCSV = function(object) {
   var date = month + '-' + day + '-' + year;
   var time = hours + ':' + minutes + ':' + seconds;
   
+  return [date,time,today];
+};
+
+// use xray to get links to tshirts
+var getShirtLinks = function() {
+  xray('http://www.shirts44mike.com/shirts.php', '.products', ['a@href'])(function(error,data) {
+      if (data) {
+        getShirtData(data);
+      } else {
+        // log any errors to error log file and console
+        // each error will be added as a new line
+        var timeStamp = getDateTime("datetime");
+        var message = "error getting t-shirt links: " + error + " at " + timeStamp[2] + '\n';
+        fs.appendFile('scraper-error.log', message, function(error) {
+          if (error) {
+            console.log ('error log not written for some reason');
+          }
+        });
+        console.log("error getting t-shirt links: " + error);
+      }
+    }
+  );
+};
+
+// iterate through list of links to get required data for each tshirt
+// async will iterate through each link, calling the shirtDataCall function on each one
+// exportToCSV function will run after async has iterated through all list items
+var getShirtData = function(links) {
+  async.each(links, shirtDataCall, function(err) {
+    exportToCSV(shirtData);
+  });
+};
+
+// use xray to get required data for each tshirt
+// finished parameter represents a callback function to let async know when all items are iterated through
+// it is returned with null to indicate there were no errors
+var shirtDataCall = function(link, finished) {
+  xray(link, {
+        image: '.shirt-picture img@src',
+        price: '.price',
+        title: '.shirt-details h1 | slice:4'
+      })(function(error,data) {
+        if (data) {
+          // add url for the page to the data object and push data object to the shirtData array
+          data.url = link;
+          shirtData.push(data);
+          return(finished(null));
+        } else {
+          // log any errors to error log file and console
+          // each error will be added as a new line
+          var timeStamp = getDateTime("datetime");
+          var message = "error getting t-shirt data: " + error + " at " + timeStamp[2] + '\n';
+          fs.appendFile('scraper-error.log', message, function(error) {
+            if (error) {
+              console.log ('error log not written for some reason');
+            }
+          });
+          console.log("error getting t-shirt data: " + error);
+        }
+      }
+    );
+};
+
+var exportToCSV = function(object) {
+  var datetime = getDateTime();
+  
+  // check if data directory exists.  if not, create it
+  // this has to be done syncronously or fs will try to write to the data directory before it is created
   try {
     fs.statSync("./data/");
   } catch(e) {
     fs.mkdirSync("./data/");
   }
-  
+  // write data for each tshirt to a csv with today's date as a file name
+  // if more than one write is done in the same day, the most current will overwrite the others
   var csvStream = csv.createWriteStream({headers: true}),
-  writableStream = fs.createWriteStream("./data/" + date + ".csv");
+  writableStream = fs.createWriteStream("./data/" + datetime[0] + ".csv");
   csvStream.pipe(writableStream);
   for (a = 0; a < object.length; a++) {
     var shirt = object[a];
-    csvStream.write({Title: shirt.title, Price: shirt.price, ImageURL: shirt.image, URL: shirt.url, Time: time});
+    csvStream.write({Title: shirt.title, Price: shirt.price, ImageURL: shirt.image, URL: shirt.url, Time: datetime[1]});
   }
   csvStream.end();
-  console.log('web crawl completed at: ' + time);
+  
+  // when finished, indicate that on the console and clear the shirtData array
+  console.log('web crawl completed at: ' + datetime[1]);
   shirtData = [];
 };
 
+// initalizes the application and runs it every 24 hours (86,400,000ms)
 function scraper() {
   getShirtLinks();
-  setTimeout(scraper, 5000);
+  setTimeout(scraper, 86400000);
 }
 
 scraper();
